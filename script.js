2d252a696695faa33cb7f5309679ba6bd1c94318
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-24 09:53:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -91,10 +91,133 @@ const initializer = () => {
     button.classList.add("letters");
     // Number to ASCII[A-Z]
     button.innerText = String.fromCharCode(i);
+    // Character button click
+    button.addEventListener("click", () => {
+      let charArray = chosenWord.split("");
+      let dashes = document.getElementsByClassName("dashes");
+      // If array contains clicked value replace the matched dash with letter else draw on canvas
+      if (charArray.includes(button.innerText)) {
+        charArray.forEach((char, index) => {
+          // If character in array is same as clicked button
+          if (char === button.innerText) {
+            // Replace dash with letter
+            dashes[index].innerText = char;
+            // Increment counter
+            winCount += 1;
+            // If winCount equals word length
+            if (winCount == charArray.length) {
+              resultText.innerHTML = `<h2 class='win-msg'>You Win!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+              // Block all buttons
+              blocker();
+            }
+          }
+        });
+      } else {
+        // Increment incorrect guess count
+        count += 1;
+        // Draw part of the hangman
+        drawMan(count);
+        // Count == 6 because head, body, left arm, right arm, left leg, right leg
+        if (count == 6) {
+          resultText.innerHTML = `<h2 class='lose-msg'>You Lose!!</h2><p>The word was <span>${chosenWord}</span></p>`;
+          blocker();
+        }
+      }
+      // Disable clicked button
+      button.disabled = true;
+    });
     letterContainer.append(button);
   }
 
   displayOptions();
+  // Call to canvasCreator (for clearing previous canvas and creating initial canvas)
+  let { initialDrawing } = canvasCreator();
+  // initialDrawing would draw the frame
+  initialDrawing();
+};
+
+// Canvas
+const canvasCreator = () => {
+  let context = canvas.getContext("2d");
+  context.beginPath();
+  context.strokeStyle = "#000";
+  context.lineWidth = 2;
+
+  // For drawing lines
+  const drawLine = (fromX, fromY, toX, toY) => {
+    context.moveTo(fromX, fromY);
+    context.lineTo(toX, toY);
+    context.stroke();
+  };
+
+  const head = () => {
+    context.beginPath();
+    context.arc(70, 30, 10, 0, Math.PI * 2, true);
+    context.stroke();
+  };
+
+  const body = () => {
+    drawLine(70, 40, 70, 80);
+  };
+
+  const leftArm = () => {
+    drawLine(70, 50, 50, 70);
+  };
+
+  const rightArm = () => {
+    drawLine(70, 50, 90, 70);
+  };
+
+  const leftLeg = () => {
+    drawLine(70, 80, 50, 110);
+  };
+
+  const rightLeg = () => {
+    drawLine(70, 80, 90, 110);
+  };
+
+  // Initial frame
+  const initialDrawing = () => {
+    // Clear canvas
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+    // Bottom line
+    drawLine(10, 130, 130, 130);
+    // Left line
+    drawLine(10, 10, 10, 131);
+    // Top line
+    drawLine(10, 10, 70, 10);
+    // Small top line
+    drawLine(70, 10, 70, 20);
+  };
+
+  return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
+};
+
+// Draw the man
+const drawMan = (count) => {
+  let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+  switch (count) {
+    case 1:
+      head();
+      break;
+    case 2:
+      body();
+      break;
+    case 3:
+      leftArm();
+      break;
+    case 4:
+      rightArm();
+      break;
+    case 5:
+      leftLeg();
+      break;
+    case 6:
+      rightLeg();
+      break;
+    default:
+      break;
+  }
 };
 
 // New Game
